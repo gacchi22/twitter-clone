6a1a4146c448d474dbdbfac3f3ecbda1ce16a299
Ubuntu <ubuntu@ip-172-31-18-9.ap-northeast-1.compute.internal>
2022-12-04 09:41:21 +0000

    2nd commit

---
+++ database/migrations/2022_08_21_002953_create_tweets_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTweetsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tweets', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->integer('user_id');
+            $table->text('tweet');
+            $table->text('image_url')->nullable();
+            $table->timestamps();
+            $table->text('created_user');
+            $table->text('update_user');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tweets');
+    }
+}
